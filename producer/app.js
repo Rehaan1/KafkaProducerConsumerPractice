2f72377f417ddaf073b59d198d24a67e7fbb89e4
Rehaan Mazid <38107493+Rehaan1@users.noreply.github.com>
2022-05-30 15:55:32 +0530

    feat: produucer written

--- producer/app.js
+++ producer/app.js
@@ -1,1 +1,26 @@
 console.log('producer....')
+
+var Kafka = require('node-rdkafka')
+
+var stream = Kafka.Producer.createWriteStream({
+    'metadata.broker.list': 'localhost:9092'
+  }, {}, {
+    topic: 'test'
+  });
+
+function queueMessage()
+{
+    const success = stream.write(Buffer.from('hi'))
+    if(success)
+    {
+        console.log('Message Wrote Successfully to Topic')
+    }
+    else
+    {
+        console.log('Something went wrong')
+    }
+}
+
+setInterval(()=>{
+    queueMessage()
+},3000)
