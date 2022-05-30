9cb8845c1d3631ff44fdbd9dac92832146e32ac3
Rehaan Mazid <38107493+Rehaan1@users.noreply.github.com>
2022-05-30 16:03:22 +0530

    feat: consumer done

--- consumer/app.js
+++ consumer/app.js
@@ -1,1 +1,19 @@
 console.log('consumer....')
+
+var Kafka = require('node-rdkafka')
+
+var consumer = Kafka.KafkaConsumer({
+    'group.id': 'kafka',
+    'metadata.broker.list': 'localhost:9092'
+  }, {});
+
+consumer.connect() // Connect To Broker
+
+consumer.on('ready', () =>{
+    console.log('consumer ready...')
+
+    consumer.subscribe(['test']) // Subscribe to Topic
+    consumer.consume()
+}).on('data', (data) => { // When Data Comes in event
+    console.log(`Messaged Received: ${data.value}`)
+})
